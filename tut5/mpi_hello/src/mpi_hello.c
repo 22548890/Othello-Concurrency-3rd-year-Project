5dfad988fc247e9f915b08feba8d59b62d398c3d
Sass, MF, Mr {22548890@sun.ac.za} <22548890@sun.ac.za>
2022-04-11 21:26:37 +0200

    hello

--- tut5/mpi_hello/src/mpi_hello.c
+++ tut5/mpi_hello/src/mpi_hello.c
@@ -19,29 +19,36 @@ int main(void) {
 	int my_rank = 0, comm_sz = 0;
 
 	/* TODO: Start up MPI */
+	MPI_Init(NULL, NULL);
 	/* TODO: Get the number of processes */
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 	/* TODO: Get my rank among all the processes */
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
 	if (my_rank != 0) {
 		/* Create greeting message for process my_rank */
 		sprintf(greeting, "Greetings from process %d of %d!", my_rank, comm_sz);
 		/* TODO: Uncomment this line once parallelised - do not change it */
 		//log_msg(greeting); 
-
+		MPI_Send(greeting, strlen(greeting)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 		/* TODO: Send message to process 0 */
 
 	} else {
 		sprintf(greeting, "Greetings from process %d of %d!", my_rank, comm_sz);
 		/* TODO: Uncomment these lines once parallelised - do not change it */
 		//log_msg(greeting); 
-		//printf("%s\n", greeting);
+		printf("%s\n", greeting);
 
 		/* TODO: Receive a message from each of the other processes and print it*/
 			/* TODO: Uncomment this line once parallelised - do not change it */
-			//printf("%s\n", greeting);
+			for (int i = 1; i < comm_sz; i++) {
+				MPI_Recv(greeting, MAX, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				printf("%s\n", greeting);
+			}
 	}
 
 	/* TODO: Shut down MPI */
+	MPI_Finalize();
 
 	return 0;
 }
