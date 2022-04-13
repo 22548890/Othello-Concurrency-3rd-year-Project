4160ee883808c3101c1afff526e9f74fac6c27c5
Sass, MF, Mr {22548890@sun.ac.za} <22548890@sun.ac.za>
2022-04-13 13:08:40 +0200

    tut working

--- tut5/mpi_rs_allr/src/mpi_rs_allr.c
+++ tut5/mpi_rs_allr/src/mpi_rs_allr.c
@@ -14,20 +14,18 @@ int main(int argc, char *argv[]) {
    int rank, ranksum = 0;
 
 	/* TODO: Start up MPI */
-	/* TODO: Start up MPI */
 	MPI_Init(&argc, &argv);
 	/* TODO: Get my rank among all the processes */
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	/* TODO: Execute a broadcast reduction operation to compute the rank sum */
-	MPI_Reduce(&rank, &ranksum, 1, MPI_INT, MPI_SUM,0, MPI_COMM_WORLD);
+	MPI_Allreduce(&rank, &ranksum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 	/* TODO: If you're process 0, log the ranksum and print it 
 		- uncomment the following two lines, but do not change them */ 
 		if (rank==0){
 			log_result(ranksum);
         	printf("Process %d: the rank sum is: %d \n ", rank, ranksum);
-		}
-        //log_result(ranksum);
-        //printf("Process %d: the rank sum is: %d \n ", rank, ranksum);
+		} 
+        
 
 	/* TODO: Shut down MPI */
 	MPI_Finalize();
