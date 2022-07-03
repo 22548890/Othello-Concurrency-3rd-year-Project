78808c9ce6e44419448f1457d36d512d12b6e7e8
Markus Sass <100954643+22548890@users.noreply.github.com>
2022-07-03 21:52:26 +0200

    Delete tut4 directory

--- tut4/run.sh
+++
@@ -1,4 +0,0 @@
-#echo "./schedule_processes data/process.list 0 2"
-#./schedule_processes data/process.list 0 2 
-echo "./schedule_processes data/process.list 1 2"
-./schedule_processes data/process.list 1 2 
